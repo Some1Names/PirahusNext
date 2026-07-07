d49c58f401a49e1dbac628426e6b8441d390d53b
Some1Names <uefapeerawit@gmail.com>
2026-07-07 14:27:10 +0700

    feat: remove logout functionality and associated UI elements from LoginButton component

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { FaUser, FaSignOutAlt } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import { useUserStore } from "@/src/store/auth";
 import { Code } from "lucide-react";
@@ -9,16 +9,11 @@ import ElectricBorder from "./reactbits/effect/ElectricBorder";
 
 const LoginButton = ({ role }: { role: string | null }) => {
   const router = useRouter();
-  const { user, getUser, logout, loading } = useUserStore();
+  const { user, getUser, loading } = useUserStore();
 
   useEffect(() => {
     getUser();
   }, [getUser]);
-
-  const handleLogout = async () => {
-    await logout();
-    router.push("/auth/login");
-  };
 
   if (loading) {
     return (
@@ -35,10 +30,6 @@ const LoginButton = ({ role }: { role: string | null }) => {
         className="flex items-center gap-3 px-5 bg-[#0d0d0d] hover:bg-[#6812D2] transition-all duration-500 ease-in-out group focus:outline-none shadow-lg h-14 cursor-pointer"
       >
         <FaUser size={18} className="text-[#F1F1F1]" />
-        <div className="w-px h-5 bg-[#F1F1F1] opacity-30" />
-        <span className="text-[#F1F1F1] text-sm font-medium tracking-widest uppercase">
-          Login
-        </span>
       </button>
     );
   }
@@ -77,16 +68,6 @@ const LoginButton = ({ role }: { role: string | null }) => {
         </ElectricBorder>
       )}
 
-      <button
-        onClick={handleLogout}
-        className="flex items-center gap-3 px-5 bg-[#0d0d0d] hover:bg-[#8a3020] transition-all duration-500 ease-in-out group focus:outline-none shadow-lg h-14 cursor-pointer"
-      >
-        <FaSignOutAlt size={18} className="text-[#F1F1F1]" />
-        <div className="w-px h-5 bg-[#F1F1F1] opacity-30" />
-        <span className="text-[#F1F1F1] text-sm font-medium tracking-widest uppercase">
-          Logout
-        </span>
-      </button>
     </div>
   );
 };
